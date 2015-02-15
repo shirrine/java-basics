e51e4b136e28a3608745b406ea544bcb0480f3d3
Shirrine <shirrine@gmail.com>
2015-02-15 18:24:58 -0500

    Skipping the Android work. :-P ... Hour 24

---
+++ java-basics/src/com/java24/hour24/FarewellWorld.java
@@ -0,0 +1,20 @@
+package com.java24.hour24;
+
+/**
+ * All done! 
+ * 
+ * @author Shirrine
+ *
+ */
+public class FarewellWorld {
+	
+	/**
+	 * Adios amigos!
+	 * 
+	 * @param args unused
+	 */
+	public static void main(String[] args){
+		System.out.println("Farewell World!");
+	}
+
+}
